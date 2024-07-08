068306a7cfa0eadbcc093d3facc0d72712993cc7
Mathis ROME <mathis.rome@icloud.com>
2024-07-08 11:44:29 +0200

    first commit

---
+++ front/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
