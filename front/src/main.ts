7b24acb8d256474f5574677d0663beacddca7da6
s-kenza <kenza.schuler@gmail.com>
2024-07-09 14:24:14 +0200

    ajout de la page des tatoueurs

--- front/src/main.ts
+++ front/src/main.ts
@@ -8,6 +8,15 @@ import Lara from '@/presets/lara'
 import App from './App.vue'
 import router from './router'
 
+import Card from 'primevue/card';
+import SelectButton from 'primevue/selectbutton';
+import Button from 'primevue/button'
+import Rating from 'primevue/rating'
+import Tag from 'primevue/tag'
+import 'primeicons/primeicons.css'
+import CardComponent from './assets/components/CardComponent.vue';
+import Toast from 'primevue/toast'
+
 const app = createApp(App)
 
 app.use(createPinia())
@@ -18,5 +27,12 @@ app.use(PrimeVue, {
     pt: Lara
 });
 
+app.component("Card", Card)
+app.component("SelectButton", SelectButton)
+app.component("Button", Button)
+app.component("Rating", Rating)
+app.component("Tag", Tag)
+app.component("CardComponent", CardComponent)
+app.component("Toast", Toast)
 
 app.mount('#app')
--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import TatoueursView from '@/views/TatoueursView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/tatoueurs',
+      name: 'tatoueurs',
+      component: TatoueursView
+    }
   ]
 })
 
