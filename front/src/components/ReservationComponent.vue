a2362d90926cd507f08ff7abf4735e83770bcb0f
lorada.andre <lorada.andre@omerin.com>
2024-07-10 11:28:07 +0200

    feature/reservation_bis - Bouton d'annulation d'une réservation

--- front/src/components/ReservationComponent.vue
+++ front/src/components/ReservationComponent.vue
@@ -1,22 +1,27 @@
 <template>
   <Card class="my-5 border-l-8 border-gray-600">
     <template #content>
-      <div class="flex">
-        <div class="w-28 h-28 flex items-center justify-center">
-          <Image src="../src/assets/illu-tatoueur.jpg" class="m-auto"/>
-        </div>
-        <div class="ml-10">
-          <h2 class="mb-3 text-xl"> Réservation du flash "{{ props.nomFlash }}"</h2>
-          <div class="flex gap-6">
-            <div class="flex my-2">
-              <i class="pi pi-calendar-clock my-auto pr-1"/>
-              <p class="my-auto">{{ props.dateReservation }}</p>
-            </div>
-            <div class="flex my-2">
-              <i class="pi pi-map-marker my-auto pr-1"/>
-              <p class="my-auto">{{ props.lieuReservation }}</p>
+      <div class="flex justify-between">
+        <div class="flex">
+          <div class="w-28 h-28 flex items-center justify-center">
+            <Image src="../src/assets/illu-tatoueur.jpg" class="m-auto"/>
+          </div>
+          <div class="ml-10">
+            <h2 class="mb-3 text-xl"> Réservation du flash "{{ props.nomFlash }}"</h2>
+            <div class="flex gap-6">
+              <div class="flex my-2">
+                <i class="pi pi-calendar-clock my-auto pr-1"/>
+                <p class="my-auto">{{ props.dateReservation }}</p>
+              </div>
+              <div class="flex my-2">
+                <i class="pi pi-map-marker my-auto pr-1"/>
+                <p class="my-auto">{{ props.lieuReservation }}</p>
+              </div>
             </div>
           </div>
+        </div>
+        <div class="my-auto">
+          <Button label="Annuler ma réservation" severity="danger" @click="cancelReservation"/>
         </div>
       </div>
 
@@ -33,5 +38,9 @@ const props = defineProps({
   dateReservation: String,
   lieuReservation: String,
 })
+
+const cancelReservation = () => {
+  //TODO: Annuler la réservation
+}
 
 </script>
