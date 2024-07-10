34c2a2ae4832b9f56094003f7489229319dddadf
BlueSkunka <quentin.somveille3874@gmail.com>
2024-07-10 10:35:18 +0200

    feat: geoloc and filters

--- back/src/models/flash.ts
+++ back/src/models/flash.ts
@@ -8,6 +8,10 @@ interface IFlash {
     name: string;
     description: string;
     price: number;
+    location: {
+        type: { type: String },
+        coordinates: [Number],
+    };
 }
 
 const flashSchema = new Schema<IFlash>({
@@ -18,6 +22,17 @@ const flashSchema = new Schema<IFlash>({
     name: { type: String, required: true, trim: true },
     description: { type: String, required: true, trim: true },
     price: { type: Number, required: true, trim: true },
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'], // 'location.type' must be 'Point'
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    }
 })
 
 const Flash = model<IFlash>('flashs', flashSchema);
